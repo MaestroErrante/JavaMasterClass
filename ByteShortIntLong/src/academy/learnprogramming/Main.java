766e16e72cf182b55e0238a5eb3fe179b65ae895
Rudyard <rudyard90@gmail.com>
2021-09-24 08:28:50 -0500

    Add primitive data type: byte

--- ByteShortIntLong/src/academy/learnprogramming/Main.java
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -10,8 +10,15 @@ public class Main {
         int myMaxIntValue = Integer.MAX_VALUE;
 
         System.out.println("Integer Minimum Value = " + myMinIntValue);
-        System.out.println("Integer Maximun Value = " + myMaxIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
         System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
         System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMacByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMacByteValue);
     }
 }
