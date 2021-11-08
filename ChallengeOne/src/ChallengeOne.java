89aecf02aaadf445fb323523dda5533615e985e8
Rudyard <rudyard90@gmail.com>
2021-11-08 08:11:30 -0600

    Challenge using primitive types

--- ChallengeOne/src/ChallengeOne.java
+++ ChallengeOne/src/ChallengeOne.java
@@ -1,4 +1,18 @@
 package ChallengeOne.src;
 
 public class ChallengeOne {
+
+    public static void main(String[] args) {
+
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+
+        short shortTotal = (short) (1000 + 10 * (byteValue + shortValue + intValue));
+        
+    }
+
 }
