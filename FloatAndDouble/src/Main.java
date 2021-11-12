4d8adfa28919f48203fa43a09a3cc7f5d5b3a6bb
Rudyard <rudyard90@gmail.com>
2021-11-12 07:06:02 -0600

    Add example and difference between int, float and double, cast double to float just to know how.

--- FloatAndDouble/src/Main.java
+++ FloatAndDouble/src/Main.java
@@ -14,5 +14,17 @@ public class Main {
         double myMaxDoubleValue = Double.MAX_VALUE;
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = (float) 5.25; /*Casting double to float*/
+        double myDoubleValue = 5.25;
+
+        /* Indicating type instead of casting
+        float myFloatValue = 5.25f;
+        */
+
+
+
+
     }
 }
