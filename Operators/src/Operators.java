de8b5c82ae93f5aafed7243f5cf64d46477eec4b
Rudyard <rudyard90@gmail.com>
2021-11-29 20:46:31 -0600

    Add if and logical operator &&

--- Operators/src/Operators.java
+++ Operators/src/Operators.java
@@ -45,6 +45,22 @@ public class Operators {
         result -= 2; // 10 - 2 = 8
         System.out.println("10 - 2 = " + result);
 
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
     }
 
 }
