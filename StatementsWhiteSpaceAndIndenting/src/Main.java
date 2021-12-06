ef7fb201b8c7763613925b2eb2351d5b63f167b2
Rudyard <rudyard90@gmail.com>
2021-12-06 13:38:40 -0600

    Generally the line is the statement which have the expression required

--- StatementsWhiteSpaceAndIndenting/src/Main.java
+++ StatementsWhiteSpaceAndIndenting/src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        //An statment doesnt have to be in one line
+
+        System.out.println("This is" +
+                "another" +
+                "still more");
+
+        //You can have multiple statements in one line but is not recommended
+        //White space is the space in between some of your expressions
+        //Another common place to put spaces is in between variable names and operators
+        // Identation is identing your code so that it's more readable 
+    }
+
 }
