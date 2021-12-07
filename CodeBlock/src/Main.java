89c5a2de34ed84b462cd31e912f6df4e1197d75b
Rudyard <rudyard90@gmail.com>
2021-12-07 10:08:24 -0600

    Declare values

--- CodeBlock/src/Main.java
+++ CodeBlock/src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+
+    public static void main (String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+    }
+
 }
