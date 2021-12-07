af2f4c6a2d8bde30a045964b56b4b4cd6b608479
Rudyard <rudyard90@gmail.com>
2021-12-07 11:29:11 -0600

    Delete duplicates and implement method

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -1,32 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+       
+        calculateScore(true, 800,5,100);
 
-        calculateScore();
+        calculateScore(true, 10000, 8, 200);
 
-        score = 1000;
-        levelCompleted = 8;
-        bonus = 200 ;
-
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
     }
 
-    public static void calculateScore() {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
 
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
-            finalScore += 1000;
+            finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
         }
     }
