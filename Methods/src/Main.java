06233eae69f5cc8efbf9e3cec1e01e801ecabdd6
Rudyard <rudyard90@gmail.com>
2021-12-07 11:17:07 -0600

    Create method and add code block

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -21,4 +21,17 @@ public class Main {
             System.out.println("Your final score was " + finalScore);
         }
     }
+
+    public static void calculateScore() {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+    }
 }
