5d53fee0e5355ceabeea3e0c8ee5b632e4310cd6
Rudyard <rudyard90@gmail.com>
2022-01-11 07:32:25 -0600

    Add new highscore

--- Methods/src/Main.java
+++ Methods/src/Main.java
@@ -5,7 +5,13 @@ public class Main {
        int highScore = calculateScore(true, 800,5,100);
         System.out.println("Your final score was " + highScore);
 
-        calculateScore(true, 10000, 8, 200);
+        boolean gameOver = true;
+        int score = 10000;
+        int levelCompleted = 8;
+        int bonus = 200;
+
+        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was "+ highScore);
 
     }
 
